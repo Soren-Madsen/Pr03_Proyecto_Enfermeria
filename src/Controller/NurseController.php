d7d18f3ddb9178f44c5833baa750793e5bfa1113
patricia <patricia.gsbcn@gmail.com>
2025-11-01 19:07:33 +0100

    change origin

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -14,6 +14,7 @@ use Doctrine\ORM\EntityManagerInterface;
 #[Route('/nurse')]
 final class NurseController extends AbstractController
 {
+    //Sin test
     private NurseRepository $nurseRepository;
     private EntityManagerInterface $entityManager;
 
