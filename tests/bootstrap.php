c927fa1c202b6fa0faa8e0090e0b3d1fd3ecba28
itsfirestorm <marcms244@gmail.com>
2025-11-05 17:24:28 +0100

    Pointed bootstrap.php to the right file (hopefully)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
+if (method_exists(Dotenv::class, 'bootEnv') && file_exists(dirname(__DIR__) . '/.env.test')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
 }
 
